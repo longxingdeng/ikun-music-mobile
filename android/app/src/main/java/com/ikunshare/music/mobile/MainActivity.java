7d6fa03bf068499d95411d79fddfee1b2a55a2f9
longxingdeng <573978533@QQ.COM>
2025-07-26 00:15:21 +0800

    fix: 修复MainActivity编译错误
    - 移除不兼容的ReactActivity方法
    - NavigationActivity不需要createReactActivityDelegate和getMainComponentName方法
    - 保留基本的onCreate方法和日志记录

--- android/app/src/main/java/com/ikunshare/music/mobile/MainActivity.java
+++ android/app/src/main/java/com/ikunshare/music/mobile/MainActivity.java
@@ -2,9 +2,6 @@ package com.ikunshare.music.mobile;
 
 import android.os.Bundle;
 import com.reactnativenavigation.NavigationActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends NavigationActivity {
 
@@ -19,28 +16,5 @@ public class MainActivity extends NavigationActivity {
     } catch (Exception e) {
       android.util.Log.e("MainActivity", "Error in MainActivity onCreate", e);
     }
-  }
-
-  /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
-   */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
-  }
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "ikunMusicMobile";
   }
 }
